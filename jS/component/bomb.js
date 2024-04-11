d56c418ef6122cfa91f8495ff88b03944f1776b0
mamadbah <bahmamadoubobosewa@gmail.com>
2024-04-11 15:52:29 +0000

    j'ai oublié de push tout ce que j'mergais tout ce temps

--- jS/component/bomb.js
+++ jS/component/bomb.js
@@ -2,38 +2,32 @@ import { originGrid } from "./grid.js"
 
 export class Bomb {
     constructor() {
-        delay: 2000 //en millisecond
+        this.max = 20
+        this.delay = 2000 // en milliseconde
     }
 
     poserBomb(position) {
-        const divs = document.querySelector('main').querySelectorAll('div')
-        const iconBomb = document.createElement('img')
-        iconBomb.src = "assets/bomb/bomb.png"
-        iconBomb.className = "bomb"
-        if (divs[position].innerHTML == '') {
-            divs[position].appendChild(iconBomb)
+        if (this.max > 0) {
+            const divs = document.querySelector('main').querySelectorAll('div')
+            const iconBomb = document.createElement('img')
+            iconBomb.src = "assets/bomb/bomb.png"
+            iconBomb.className = "bomb"
+            if (divs[position].innerHTML == '') {
+                divs[position].appendChild(iconBomb)
+            }
+            setTimeout(() => {
+                this.#exploserBomb(divs, position)
+    
+            }, this.delay)
+            this.max--;
+        } else {
+            alert("Stock de bombe épuisé")
         }
-        setTimeout(() => {
-            this.#exploserBomb(divs, position)
-
-        }, 2000)
-    }
-
-    #boom(node) {
-        node.textContent = '💥'
-        requestAnimationFrame(() => {
-            this.#animateExplo(node, 25)
-        })
-        setTimeout(() => {
-            node.textContent = ''
-            node.className = 'c'
-        }, 100)
     }
 
     #exploserBomb(nodes, position) {
-        nodes[position].removeChild(nodes[position].firstChild)
-        // mis a jour des formes a droite
-        console.log(nodes[position + 1].classList);
+        nodes[position].removeChild(nodes[position].firstChild) 
+     // Cassage des murs etc
         if (nodes[position + 1].className == 'c' || nodes[position + 1].className == 'm') {
             this.#boom(nodes[position + 1])
             originGrid[Math.floor((position + 1) / 15)][(position + 1) % 15] = 'c'
@@ -52,6 +46,17 @@ export class Bomb {
             originGrid[Math.floor((position + 15) / 15)][(position + 15) % 15] = 'c'
             this.#boom(nodes[position + 15])
         }
+    }
+
+    #boom(node) {
+        node.textContent = '💥'
+        requestAnimationFrame(() => {
+            this.#animateExplo(node, 25)
+        })
+        setTimeout(() => {
+            node.textContent = ''
+            node.className = 'c'
+        }, 100)
     }
 
     #animateExplo(node, taille) {
